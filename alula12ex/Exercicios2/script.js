283b2d27fa4006a6de68dcc0d131377cbebdf20d
Saymon Fabiane <saymon.fabiane@gmail.com>
2024-10-14 20:43:33 -0300

    update

---
+++ alula12ex/Exercicios2/script.js
@@ -0,0 +1,20 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        //bom dia
+        img.src = 'midia/manhã.png'
+        document.body.style.backgroundColor = '#e2cd9f'
+    }else if (hora >= 12 && hora < 18){
+        //boa tarde
+        img.src = 'midia/tarde.png'
+        document.body.style.backgroundColor = '#b9846f'
+    }else{
+        // boa noite
+        img.src = 'midia/noite.png'
+        document.body.style.backgroundColor = '#515154'
+    }
+}
