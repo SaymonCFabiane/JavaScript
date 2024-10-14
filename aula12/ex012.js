d60772b2cb8d35384bdff2f88c3e857941039a86
Saymon Fabiane <saymon.fabiane@gmail.com>
2024-10-14 14:28:16 -0300

    Create ex012.js

---
+++ aula12/ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if(hora >= 6 && hora < 12){
+    console.log(`Bom dia!`)
+} else if (hora >= 12 && hora <=18){
+    console.log(`Boa tarde!`)
+} else if(hora > 22 || hora <=6) {
+    console.log(`Boa madrugada!`)
+}else{
+    console.log('Boa noite!')
+}
