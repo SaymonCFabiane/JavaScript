6aae25f1ef2b6a3b0eb5aa0efa7c25cbe5b21a06
Saymon Fabiane <saymon.fabiane@gmail.com>
2024-10-18 11:03:34 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,7 +1,7 @@
 let num = [5, 8, 2, 9, 3] 
-//num [5] = 9
-
-//num.sort()
+/*
+num [5] = 9
+num.sort()
 for(let i = 0; i<3; i++){
     console.log(`O numero é: ${num[i]}`)
 }
@@ -10,10 +10,10 @@ for(i = 0; i<num.length; i++){
     console.log(`O numero é: ${num[i]}`)
 }
 for(i = 0; i<num.length; i++){
-    console.log(`O posição ${i} tem o valor ${num[i]}`)
-}
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
+}*/
+console.log("Separação")
 for(let i in num){
+    console.log(`A posição ${i} tem o valor ${num[i]}`)
+    } 
 
-    console.log()
-    
-    }
