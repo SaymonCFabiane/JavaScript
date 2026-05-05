58815b7ab3e9278bfdc9b05511aba62b0a82b9f5
Saymon Fabiane <saymon.fabiane@gmail.com>
2026-05-05 15:35:19 -0300

    1.4

---
+++ MatheusBattisti/27_metodos_string/js/scripts.js
@@ -0,0 +1,25 @@
+// length
+
+var nome = "Matheus";
+
+console.log(nome.length);
+
+var obj = "Bola";
+
+console.log(obj.length);
+
+// indexOf
+
+console.log(nome[2]);
+
+var frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("roeu"));
+
+roeu = frase.slice(7, 11);
+
+console.log(roeu);
+
+var novaFrase = frase.replace("roeu", "testou");
+
+console.log(novaFrase);
