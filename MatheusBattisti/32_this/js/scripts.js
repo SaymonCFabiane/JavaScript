57c73ba6c956170b504daa206f97ee4138b0d966
Saymon Fabiane <saymon.fabiane@gmail.com>
2026-05-07 15:29:56 -0300

    1.6

--- MatheusBattisti/32_this/js/scripts.js
+++ MatheusBattisti/32_this/js/scripts.js
@@ -1,11 +1,36 @@
 var teste = 5;
 
 console.log(this);
+console.log(this.teste);
+console.log(this.teste);
 
 let pessoa = {
   nome: "Matheus",
   idade: 29,
   falar: function () {
     console.log("Olá, tudo bem?");
+  },
+  dizerNome: function () {
+    console.log("O nome é " + this.nome);
+  },
+  aniversario: function () {
+    this.idade += 1;
+  },
+  saudacao: function(){
+    return "Sr. " + this.nome;
   }
 };
+
+pessoa.dizerNome();
+
+console.log(pessoa.idade);
+
+pessoa.aniversario();
+
+console.log(pessoa.idade);
+
+var sdc = pessoa.saudacao();
+
+console.log("Olá, " + sdc);
+
+
