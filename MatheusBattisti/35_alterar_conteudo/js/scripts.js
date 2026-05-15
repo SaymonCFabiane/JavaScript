ee4c62069ebc4a6619445a0eeaaeebde288e06ae
Saymon Fabiane <saymon.fabiane@gmail.com>
2026-05-15 16:49:02 -0300

    1.7

---
+++ MatheusBattisti/35_alterar_conteudo/js/scripts.js
@@ -0,0 +1,10 @@
+// selecionar elemento
+var title = document.querySelector("#title");
+
+// innerHTML
+title.innerHTML = "Testando o texto alterado!";
+
+// textContent -> mais utilizado, recomendado e performático
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.textContent = "Testando o textContent";
